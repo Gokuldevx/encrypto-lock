0caa6170cd0756a2c0d7d03b0b598c1979e10079
Gokuldev <gokuldevse2001@gmail.com>
2025-03-04 10:25:29 +0530

    Updated file

--- src/lucide-react.d.ts
+++ src/lucide-react.d.ts
@@ -3,5 +3,6 @@ declare module 'lucide-react' {
   
     export const Loader2: FC<SVGProps<SVGSVGElement>>;
     export const Mail: FC<SVGProps<SVGSVGElement>>;
+    export const LogIn: FC<SVGProps<SVGSVGElement>>;
     // Add other icons you use from lucide-react here
   }
