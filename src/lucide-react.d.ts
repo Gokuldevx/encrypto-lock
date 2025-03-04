7dda418d06a31aae1811a85c35a860d0f4069115
Gokuldev <gokuldevse2001@gmail.com>
2025-03-04 22:00:04 +0530

    Added few Icons

--- src/lucide-react.d.ts
+++ src/lucide-react.d.ts
@@ -4,5 +4,12 @@ declare module 'lucide-react' {
     export const Loader2: FC<SVGProps<SVGSVGElement>>;
     export const Mail: FC<SVGProps<SVGSVGElement>>;
     export const LogIn: FC<SVGProps<SVGSVGElement>>;
+    export const X: FC<SVGProps<SVGSVGElement>>;
+    export const LockIcon: FC<SVGProps<SVGSVGElement>>;
+    export const UserIcon: FC<SVGProps<SVGSVGElement>>;
+    export const ShieldCheckIcon: FC<SVGProps<SVGSVGElement>>;
+    export const ClipboardListIcon: FC<SVGProps<SVGSVGElement>>;
+    export const KeyIcon: FC<SVGProps<SVGSVGElement>>;
+    export const BellIcon: FC<SVGProps<SVGSVGElement>>;
     // Add other icons you use from lucide-react here
   }
