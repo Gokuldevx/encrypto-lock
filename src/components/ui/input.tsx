ed4199d3499fa8bbe0db02fe8cb32b9d764d7b4f
Gokuldev <gokuldevse2001@gmail.com>
2025-03-04 10:26:39 +0530

    Improved UI/UX

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,5 @@
-import * as React from "react"
-
-import { cn } from "../../lib/utils"
+import * as React from "react";
+import { cn } from "../../lib/utils";
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
@@ -8,15 +7,15 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "flex h-10 w-full rounded-md border border-[#f66435] bg-[#f4efca] hover:border-[#f4efca] hover:bg-[#f66435] px-3 py-2 text-[#f66435] hover:text-[#f4efca] placeholder-[#f66435] hover:placeholder-[#f4efca] ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#f66435] focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           className
         )}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
