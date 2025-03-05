859e892b1bcc18982ccc5aca3ddc8f3c7b91d827
Gokuldev <gokuldevse2001@gmail.com>
2025-03-05 17:50:34 +0530

    Added Homepage for Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: "/encrypto-lock/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Moves all node_modules dependencies into a separate "vendor.js"
+          }
+        },
+      },
+    },
+  },
 })
